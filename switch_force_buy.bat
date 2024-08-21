8db600011287ab5cda778fcb7ab02c97c91c2810
2kliksAlex <4kliksAlex@YouTube>
2024-08-21 21:10:14 +0800

    v1.2.2

--- switch_force_buy.bat
+++ switch_force_buy.bat
@@ -5,9 +5,9 @@ set "cs_path=C:\Program Files (x86)\Steam\steamapps\common\Half-Life\cstrike"
 set /p first_line=<"%cs_path%\autobuy.txt"
 
 if "!first_line!"=="// Force-buy" (
-    copy /y "cstrike\autobuy_full.txt" "%cs_path%\autobuy.txt" >nul
+    copy /y "autobuy_full.txt" "%cs_path%\autobuy.txt" >nul
 ) else (
-    copy /y "cstrike\autobuy_force.txt" "%cs_path%\autobuy.txt" >nul
+    copy /y "autobuy_force.txt" "%cs_path%\autobuy.txt" >nul
 )
 
 set /p first_line=<"%cs_path%\autobuy.txt"
